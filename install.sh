b4a11d3444ac1497add0619635f7be19e4202170
Ben Tuxworth <benjamin.j.tuxworth@gmail.com>
2025-12-19 11:39:30 +0000

    Change red, green & yellow ANSI codes

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 set -e
 
 # ANSI colours
-red="\033[31m"
-green="\033[32m"
-yellow="\033[33m"
+red="\033[91m"
+green="\033[92m"
+yellow="\033[93m"
 reset="\033[0m"
 
 echo "${green}=======================================${reset}"
